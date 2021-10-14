629f7483c08d497ae08f69f87566e2b6a4195002
Beskow <beskow@pop-os.localdomain>
2021-10-14 11:45:08 +0200

    Created fetch function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import './App.css';
+import { useState, useEffect} from 'react'
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState<string[]>([])
+
+  const url = 'https://api.themoviedb.org/3/trending/all/day?api_key=';
+  const key = '0493315db205d26d27cfee3b1956d4d0'
+
+  const fetchMovies = async() => {
+    try {
+      const response = await fetch(url + key)
+      const json = await response.json()
+      setMovies(json.results)
+      
+    } catch(err) {
+      console.error(err);
+    }
+  }
+  console.log(movies)
+  useEffect(() => {
+    fetchMovies()
+  }, [])
+
   return (
-    <section className="App">
-     
+    <section>
+      <header>Movie Flex</header>
+      <main>
+
+      </main>
     </section>
-  );
+  )
 }
 
-export default App;
+export default App
